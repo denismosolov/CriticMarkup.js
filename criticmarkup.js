14ebcf630a93e2f9360b61caedfaba3b5cbde5c1
Denis Mosolov <denismosolov@gmail.com>
2014-07-01 21:56:01 +0400

    * improved regexp for deletions

--- criticmarkup.js
+++ criticmarkup.js
@@ -3,8 +3,14 @@ var CriticMarkup = {
 		return markup.replace(/\{~~/g, '<del>')
 		.replace(/~>/g, '</del><ins>')
 		.replace(/~~}/g, '</ins>')
-		.replace(/\{\-\-/g, '<del>')
-		.replace(/\-\-\}/g, '</del>')
+		.replace(/\{\-\-([\s\S]*?)\-\-[ \t]*(\[([\s\S]*?)\])?[ \t]*\}/gm, function(match, contents){
+			if (contents.match(/(\r\n|\n|\r)/gm)) {
+				return '<del>&nbsp;</del>';
+			} else {
+				return '<del>' + contents.replace(/(\r\n|\n|\r)/gm, "&nbsp;") + '</del>';
+			}
+		})
+		.replace(/\{\+\+/g, '<ins>')
 		.replace(/\{\+\+/g, '<ins>')
 		.replace(/\+\+\}/g, '</ins>')
 		.replace(/\{>>/g, '<span class="critic comment">')
